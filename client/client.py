f2a63123cb3ed942448a8203767d0c1bd0ee26ab
sgilz <sgilz@eafit.edu.co>
2020-10-25 23:11:31 -0500

    all commands implemented

--- client/client.py
+++ client/client.py
@@ -1,21 +1,30 @@
-from io import StringIO
+#std lib packages
+from io import StringIO, BytesIO
+import os
+import os.path as path
 import socket
 import sys
+#user-defined packages
 from util.parser import Parser
 
 class Client:
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #http_bind_address
-        self.BUFFER_SIZE = 4096
+        self.BUFFER_SIZE = 1024
+        self.parser = Parser()
     
-    def run(self):
-        self.connect()
+    def run(self, address = None, port = 9000):
+        """
+        Tries to connect to the server deployed on the given access point (address:port).
+        and  start a session with it.
+        """
+        #set default address if it's not given
+        address = socket.gethostname() if address == None else address
+        self.__connect( address, port)
         self.__session()
 
-    def connect(self, address = None, port = 9000):
-        #set default address it's not given
-        address = socket.gethostname() if address == None else address
+    def __connect(self, address, port):
         print(f"# Connecting to {address}:{port}")
         try:
             self.socket.connect((address, port))
@@ -32,32 +41,110 @@ class Client:
         while True:
             try:
                 msg = input("> ").strip()
-                parser = Parser(msg)
-                if msg and parser.parse():
-                    self.send(msg)
+                if msg and self.parser.parse(msg):
+                    self.__execute(msg)
                 else:
-                    print(f"# Invalid command: '{msg}', type 'help' if you need a hand")
+                    print(f"# Invalid command: '{msg}', type 'help' if you need a hand.")
             except (KeyboardInterrupt, EOFError):
                 print("# Disconnected!")
                 self.socket.close()
                 sys.exit()
-            except InterruptedError:
+            except (InterruptedError, ConnectionError):
                 print(f"# Disconnected due interrupted connection. :(")
+                self.socket.close()
                 sys.exit(1)
 
-    def send(self, msg):
+    def __execute(self, msg):
+        instruction = self.parser.instruction(msg)
         if msg == "quit": 
             raise KeyboardInterrupt
         elif msg == "help":
             print(self.__help())
+        elif instruction == "up":
+            self.__send_file(msg)
+        elif instruction == "down":
+            self.__receive_file(msg)
         else:
             self.socket.send(msg.encode())
-            data = self.socket.recv(self.BUFFER_SIZE)
-            if data:
-                line = data.decode('UTF-8')    # convert to string (Python 3 only)
-                #line = line.replace("\n","")   # remove newline character
-                print("< " + line )
-            else: raise InterruptedError
+        self.__listen(msg)
+    
+    def __send_file(self, msg):
+        """
+        Sends the file which is in the given path.
+        """
+        self.socket.send(msg.encode()) # advertisement
+
+        bucket_name, file_path = self.parser.args(msg)
+        if path.isfile(file_path):
+            file_name = path.basename(file_path)
+            size = path.getsize(file_path)
+            header = f"{bucket_name} {file_name} {size}"
+            sent = False
+            while not sent: # resending until get confirmation
+                self.socket.send(header.encode())
+                sent = self.socket.recv(self.BUFFER_SIZE)
+            with open(file_path, 'rb') as f:
+                print(f"# Sending {file_name}")
+                while True:
+                    bytes_read = f.read(self.BUFFER_SIZE)
+                    if not bytes_read: 
+                            break
+                    try:
+                        self.socket.sendall(bytes_read)
+                    except:
+                        print("# Error while sending the file. Please, retry.")
+                        return
+                print("# Sent")
+        else:
+            print(f"# Not found file: {file_path}")
+        
+    def __receive_file(self, msg):
+        """
+        Receives the file which is in the given path.
+        """
+        self.socket.send(msg.encode())
+        header = self.socket.recv(self.BUFFER_SIZE)
+        if header:
+            line = header.decode('UTF-8').replace('\n','')
+            if line == "not found": #  omit transmission if bucket or file doesn't exist
+                return 
+            self.socket.send("OK".encode()) # sent confirmation of header
+            args = line.split()
+            if len(args) == 3:
+                bucket_name, file_name, file_size = args
+                file_size = int(file_size)
+                print(f"# Receiving: {file_name}")
+                with BytesIO() as incoming_bytes:
+                    total_received = 0
+                    while total_received < file_size:
+                        bytes_read = self.socket.recv(self.BUFFER_SIZE)
+                        total_received += incoming_bytes.write(bytes_read)
+                        if not bytes_read:
+                            print("# Data corrupted. Dismissing...")
+                            return
+                    print("# Received")
+                    self.socket.send("OK".encode()) # sent confirmation of data
+
+                    this = path.relpath(__file__) # this files path
+                    this = path.split(this)[0] # removing <filename>.py
+                    down_path = path.join(this, "downloads", bucket_name)
+                    if not path.isdir(down_path):
+                        print(f"# '{down_path}' not found. Creating...")
+                        os.makedirs(down_path)
+                    file_path = path.join(down_path, file_name)
+                    with open(file_path, 'wb') as f:
+                        f.write(incoming_bytes.getvalue())
+            else:
+                print("# Header corrupted.")
+        else: 
+            print("# Header not found.")
+
+    def __listen(self, msg):
+        data = self.socket.recv(self.BUFFER_SIZE)
+        if data:
+            line = data.decode('UTF-8')    # convert to string (Python 3 only)
+            print("< " + line )
+        else: raise InterruptedError
 
     def __help(self):
         lines = [
@@ -69,7 +156,11 @@ class Client:
             "- help: Shows this guideline.\n",
             "- list: Lists the different existing buckets.\n",
             "- quit: Breaks the connection to the server.\n",
-            "- up <BUCKET_NAME> <FILE_NAME>: Uploads a file to an existing bucket.\n"
+            "- up <BUCKET_NAME> <FILE_NAME>: Uploads a file to an existing bucket.\n\n",
+            "* REMARKS: \n",
+            "    - The available commands are all CASE-SENSITIVE\n"
+            "    - <BUCKET_NAME> only accepts alphanumeric, numeric and '_' chars.\n",
+            "    - <FILE_NAME accepts the same as <BUCKET_NAME> + and optional aphanumeric extension.\n"
         ]
         ios = StringIO()
         ios.writelines(lines)
--- server/server.py
+++ server/server.py
@@ -1,17 +1,20 @@
-from command_handler import CommandHandler
+#std lib packages
+from io import BytesIO
 from multiprocessing import Process
 import os
 import socket
 import sys
-
+#user-defined packages
+from command_handler import CommandHandler
+from util.parser import Parser
 class Server:
     """
     This class will represent a Server entity which allows access to remote 
     buckets.
     """
-    def __init__(self, root_directory, address = None, port = 9000):
+    def __init__(self, root_directory = "buckets", address = None, port = 9000):
         self.__root_directory = root_directory
-        self.BUFFER_SIZE = 4096
+        self.BUFFER_SIZE = 1024
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #http_bind_address 
         address = socket.gethostname() if address == None else address #set default address it's not given
@@ -39,10 +42,13 @@ class Server:
             except KeyboardInterrupt:
                 print("\n# Closing connection")
                 self.__close_processes(pool, kill=True)
+                self.socket.close()
                 sys.exit()
-            except Exception as e:
+            except:
+                print(f"# Disconnected due interrupted connection. :(")
                 self.__close_processes(pool, kill=True)
-                raise e
+                self.socket.close()
+                sys.exit(1)
             finally:
                 poolcp = pool.copy()
                 for p in poolcp:
@@ -51,20 +57,102 @@ class Server:
                         p.join()
                         pool.remove(p)
     
-    def __session(self, client, address):   
+    def __session(self, client, address):
         print(f"# Connected: {address}")
-        handler = CommandHandler(self.__root_directory)
         while True:
-            data = client.recv(self.BUFFER_SIZE)
-            if data:
-                line = data.decode('UTF-8')    # convert to string (Python 3 only)
-                line = line.replace('\n','')   # remove newline character
+            msg = client.recv(self.BUFFER_SIZE)
+            if msg:
+                line = msg.decode('UTF-8').replace('\n','')# convert to string (Python 3 only)
                 print(f"< {address}: {line}")
-                response = handler.execute(line)
+                response = self.__response(line, client)
                 client.send(response.encode())
             else:
                 print(f"# Disconnected: {address}")
                 break
+        client.close()
+    
+    def __response(self, line, client):
+        """
+        Manages incoming lines
+        """
+        parser = Parser()
+        response = ""
+        if not parser.parse(line):
+            response = f"Command corrupted: {line}"
+        else:
+            handler = CommandHandler(self.__root_directory)
+            instruction = parser.instruction(line)
+            if  instruction == "up":
+                response = self.__receive_file(client, handler)
+            elif instruction == "down":
+                response = self.__send_file(line, client, handler)
+            else:
+                response = handler.execute(line)
+        return response
+        
+    def __receive_file(self, client, handler):
+        """
+        Receives the file which is in the given path.
+        """
+        header = client.recv(self.BUFFER_SIZE)
+        response = ""
+        if header:
+            line = header.decode('UTF-8').replace('\n','')
+            print(f"< header: {line}")
+            client.send("OK".encode()) # sent confirmation of header
+            args = line.split()
+            if len(args) == 3: 
+                bucket_name, file_name, file_size = args
+                file_size = int(file_size)
+                print(f"# Receiving: {file_name}")
+                with BytesIO() as incoming_bytes:
+                    total_received = 0
+                    while total_received < file_size:
+                        bytes_read = client.recv(self.BUFFER_SIZE)
+                        total_received += incoming_bytes.write(bytes_read)
+                        if not bytes_read:
+                            response = "Data corrupted."
+                            break
+                    print("# Received")
+                    response = handler.up(bucket_name, file_name, incoming_bytes)
+            else: 
+                response = f"Header corrupted: {line}."
+        else: 
+            response = "Header not found."
+        return response
+
+    def __send_file(self, line, client, handler):
+        """
+        Sends the file which is in the given path.
+        """
+        response = ""
+        bucket_name, file_name = Parser().args(line)
+        data = handler.down(bucket_name, file_name)
+        if data:
+            size = len(data)
+            header = f"{bucket_name} {file_name} {size}"
+            sent = False
+            while not sent: # resending until get confirmation
+                client.send(header.encode())
+                sent = client.recv(self.BUFFER_SIZE)
+            print(f"# Sending {file_name}")
+            with BytesIO(data) as buffer:
+                while True:
+                    bytes_read = buffer.read(self.BUFFER_SIZE)
+                    if not bytes_read: 
+                            break
+                    try:
+                        client.sendall(bytes_read)
+                    except:
+                        print("# Error while sending the file. Please, retry.")
+                        return
+            print("# Sent")
+            client.recv(self.BUFFER_SIZE)
+            response = "Success!"
+        else:
+            client.send("not found".encode())
+            response = f"Not found: {bucket_name}/{file_name}"
+        return response
 
     def __close_processes(self, pool, kill = False):
         for p in pool:
@@ -73,6 +161,18 @@ class Server:
                 p.join()
         self.socket.close()
 
+def usage():
+    return "USAGE:\n    python3 server.py [buckets_root_dir]"
+
 if __name__ == "__main__":
-    server = Server("buckets/")
-    server.start()
+    argv = sys.argv
+    argc = len(argv)
+    if argc == 1:
+        server = Server()
+        server.start()
+    elif argc == 2:
+        server = Server(argv[1])
+        server.start()
+    else:
+        print(usage())
+    
